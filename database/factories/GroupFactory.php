392027fe35b37adc94f34f7a4e7c65c1ba4946b1
andianno <andrea.iannotti@student.univaq.it>
2025-10-21 18:14:20 +0200

    Add posts relationship to User model and update PostFactory content generation

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Group>
  */
-class PostFactory extends Factory
+class GroupFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,9 +17,8 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
-            'content' => fake()->paragraph(),
-            'movies' => json_encode([1,2,3]),
+            'name' => fake()->company(),
+            'description' => fake()->paragraph(),
         ];
     }
 }
