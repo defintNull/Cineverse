dd51cbb52112e6e4ec233a0618afa7599352d632
andianno <andrea.iannotti@student.univaq.it>
2025-10-23 18:55:47 +0200

    comment

--- database/migrations/2025_10_14_171640_create_comments_table.php
+++ database/migrations/2025_10_14_171640_create_comments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('post_id');
-            $table->String('content');
+            $table->Text('content');
             $table->timestamps();
         });
     }
